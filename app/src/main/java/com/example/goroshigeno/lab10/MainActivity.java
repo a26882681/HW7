f270c52da6981db5d7ad253f990223f3847e7fe6
ac19970216 <ac19970216@gamil.com>
2018-01-05 14:46:16 +0800

    Edit display function

--- app/src/main/java/com/example/goroshigeno/lab10/MainActivity.java
+++ app/src/main/java/com/example/goroshigeno/lab10/MainActivity.java
@@ -43,4 +43,84 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    private void display(final GoogleMap googleMap){
+        String urlParkingArea = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=a880adf3-d574-430a-8e29-3192a41897a5";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                urlParkingArea,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("TAG", "response = " + response.toString());
+
+                        try {
+
+                            JSONArray data = response.getJSONObject("result").getJSONArray("results");
+                            for (int i = 0; i < data.length(); i++) {
+                                JSONObject o = data.getJSONObject(i);
+                                googleMap.addMarker(new MarkerOptions()
+                                        .position(new LatLng(o.getDouble("緯度(WGS84)"), o.getDouble("經度(WGS84)")))
+                                        .title(o.getString("停車場名稱"))
+                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.images))
+                                );
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("TAG", "error : " + error.toString());
+                    }
+                }
+        );
+        Volley.newRequestQueue(MainActivity.this).add(jsonObjectRequest);
+    }
+    private void display_filter(final GoogleMap googleMap){
+        String urlParkingArea = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=a880adf3-d574-430a-8e29-3192a41897a5";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                urlParkingArea,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("TAG", "response = " + response.toString());
+
+                        try {
+
+                            JSONArray data = response.getJSONObject("result").getJSONArray("results");
+                            for (int i = 0; i < data.length(); i++) {
+                                JSONObject o = data.getJSONObject(i);
+                                MarkerOptions m1 = new MarkerOptions();
+                                m1.position(new LatLng(o.getDouble("緯度(WGS84)"), o.getDouble("經度(WGS84)")));
+                                m1.title(o.getString("停車場名稱"));
+                                m1.draggable(true);
+                                if(o.getString("停車場名稱").contains("機車")){
+                                    m1.icon(BitmapDescriptorFactory.fromResource(R.drawable.scooter));
+                                }
+                                else if (o.getString("停車場名稱").contains("自行車")){
+                                    m1.icon(BitmapDescriptorFactory.fromResource(R.drawable.bicycle));
+                                }
+                                else  m1.icon(BitmapDescriptorFactory.fromResource(R.drawable.car));
+
+                                googleMap.addMarker(m1);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("TAG", "error : " + error.toString());
+                    }
+                }
+        );
+        Volley.newRequestQueue(MainActivity.this).add(jsonObjectRequest);
+
+
+    }
 }
